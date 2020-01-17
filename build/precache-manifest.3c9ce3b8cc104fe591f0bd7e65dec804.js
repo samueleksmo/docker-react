df9b4c47a29941b9b95d99bce16ababcec98fb9b
Samuel Eksmo <samuel.eksmo1@ibm.com>
2020-01-17 12:11:05 +0100

    change

---
+++ build/precache-manifest.3c9ce3b8cc104fe591f0bd7e65dec804.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2bc107d6e9437771c8d8013c77f5e92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dfd2849f263c7ed96a5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "31d605b83920e1cdcbb0",
+    "url": "/static/js/2.dd1fcc93.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.dd1fcc93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0dfd2849f263c7ed96a5",
+    "url": "/static/js/main.eff67f85.chunk.js"
+  },
+  {
+    "revision": "810b7eddbba2845a5756",
+    "url": "/static/js/runtime-main.4e64986e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-export default class PersonList extends React.Component {
-    state = {
-      persons: []
-    }
-  
-    componentDidMount() {
-      axios.get(`https://jsonplaceholder.typicode.com/users`)
-        .then(res => {
-          const persons = res.data;
-          this.setState({ persons });
-        })
-    }
-  
-    render() {
-      return (
-        <ul>
-          { this.state.persons.map(person => <li>{person.name}</li>)}
-        </ul>
-      )
-    }
-  }
+import logo from './logo.svg';
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Samuels! <code>src/App.js</code> !
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
