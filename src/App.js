574981b7ccdedeb64fe43939d0e7d30c757a71f2
Samuel Eksmo <samuel.eksmo1@ibm.com>
2020-01-17 11:25:21 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+export default class PersonList extends React.Component {
+    state = {
+      persons: []
+    }
+  
+    componentDidMount() {
+      axios.get(`https://jsonplaceholder.typicode.com/users`)
+        .then(res => {
+          const persons = res.data;
+          this.setState({ persons });
+        })
+    }
+  
+    render() {
+      return (
+        <ul>
+          { this.state.persons.map(person => <li>{person.name}</li>)}
+        </ul>
+      )
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
